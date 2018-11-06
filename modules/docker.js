cc19733acd751e7d7a391f1b0b7bd0043aa4a0b2
Alkeryn <im.plbraun@gmail.com>
2018-11-06 22:04:05 +0100

    fixed indent

--- modules/docker.js
+++ modules/docker.js
@@ -2,9 +2,9 @@ const Docker = require("dockerode");
 const docker = new Docker({socketPath: '/var/run/docker.sock'});
 module.exports = {
     pull: function(callback,image){
-	docker.pull(image, function (err, stream) {
-	    callback(err,stream);
-	});
+        docker.pull(image, function (err, stream) {
+            callback(err,stream);
+        });
     },
     create: function(callback,image,name){
         docker.createContainer({Image: image, Cmd: ['/bin/bash'], name: name}, function (err, container) {
@@ -14,157 +14,157 @@ module.exports = {
         });
     },
     run: function(callback,image){
-	docker.run(image, ['bash', '-c', 'ls -al /'], process.stdout, function (err, data, container) {
-	    callback(data.StatusCode);
-	});
+        docker.run(image, ['bash', '-c', 'ls -al /'], process.stdout, function (err, data, container) {
+            callback(data.StatusCode);
+        });
 
     },
     remove: function(callback,containerid){
-	var container = docker.getContainer(containerid);
-	container.remove(function (err, data) {
-	    callback(data);
-	});
+        var container = docker.getContainer(containerid);
+        container.remove(function (err, data) {
+            callback(data);
+        });
 
     },
     stop: function(callback,containerid){
-	var container = docker.getContainer(containerid);
-	container.stop(function (err, data) {
-	    callback(data);
-	});
+        var container = docker.getContainer(containerid);
+        container.stop(function (err, data) {
+            callback(data);
+        });
 
     },
     start: function(callback,containerid){
-	var container = docker.getContainer(containerid);
-	container.start(function (err, data) {
-	    callback(data);
-	});
+        var container = docker.getContainer(containerid);
+        container.start(function (err, data) {
+            callback(data);
+        });
     },
     inspect: function(callback,containerid){
-	var container = docker.getContainer(containerid);
-	container.inspect( function(err,data) {
-	callback(data)
-	});
+        var container = docker.getContainer(containerid);
+        container.inspect( function(err,data) {
+            callback(data)
+        });
     },
     list: function(callback){ //callback tous les {nom,id,etc..} de tout les docker, peut prendre tous les arguments callback doit être le premier argument
-	what=arguments
-	delete what[0]
-	docker.listContainers({all: true}, function(err, containers) {
-	var retour = {};
-	    for(a in what){
-		switch(what[a]){
-		    case "id":
-		        retour.id = [];
-			for(x in containers) {
-			    retour.id.push(containers[x].Id);
-			}
-			break;
-		    case "names":
-		        retour.names = [];
-			for(x in containers) {
-			    retour.names.push(containers[x].Names[0]);
-			}
-			break;
-		    case "image":
-                        retour.image = [];
-			for(x in containers) {
-			    retour.image.push(containers[x].Image);
-			}
-			break;
-		    case "imageid":
-                        retour.imageid = [];
-			for(x in containers) {
-			    retour.imageid.push(containers[x].ImageID);
-			}
-			break; //verified to there
-		    case "command":
-                        retour.command = [];
-			for(x in containers) {
-			    retour.command.push(containers[x].Command);
-			}
-			break;
-		    case "created":
-                        retour.created = [];
-			for(x in containers) {
-			    retour.created.push(containers[x].Created);
-			}
-			break;
-		    case "ports":
-                        retour.ports = [];
-			for(x in containers) {
-			    retour.ports.push(containers[x].Ports[0]);
-			}
-			break;
-		    case "labels":
-                        retour.labels = [];
-			for(x in containers) {
-			    retour.labels.push(containers[x].Labels);
-			}
-			break;
-		    case "state":
-                        retour.state = [];
-			for(x in containers) {
-			    retour.state.push(containers[x].State);
-			}
-			break;
-		    case "status":
-                        retour.status = [];
-			for(x in containers) {
-			    retour.status.push(containers[x].Status);
-			}
-			break;
-		    case "hostconfig":
-                        retour.hostconfig = [];
-			for(x in containers) {
-			    retour.hostconfig.push(containers[x].HostConfig);
-			}
-			break;
-		    case "networksettings":
-                        retour.networksettings = [];
-			for(x in containers) {
-			    retour.networksettings.push(containers[x].NetworkSettings);
-			}
-			break;
-		    case "mounts":
-                        retour.mounts = [];
-			for(x in containers) {
-			    retour.mounts.push(containers[x].Mounts);
-			}
-			break;
-		    case "*":
-			retour = {
-			    "id":[],
-			    "names":[],
-			    "image":[],
-			    "imageid":[],
-			    "command":[],
-			    "created":[],
-			    "ports":[],
-			    "labels":[],
-			    "state":[],
-			    "status":[],
-			    "hostconfig":[],
-			    "networksettings":[],
-			    "mounts":[],
+        what=arguments
+        delete what[0]
+        docker.listContainers({all: true}, function(err, containers) {
+            var retour = {};
+            for(a in what){
+                switch(what[a]){
+                    case "id":
+                        retour.id = [];
+                        for(x in containers) {
+                            retour.id.push(containers[x].Id);
                         }
-			for(x in containers) {
-			    retour.id.push(containers[x].Id);
-			    retour.names.push(containers[x].Names[0]);
-			    retour.image.push(containers[x].Image);
-			    retour.imageid.push(containers[x].ImageID);
-			    retour.command.push(containers[x].Command);
-			    retour.created.push(containers[x].Created);
-			    retour.ports.push(containers[x].Ports[0]);
-			    retour.labels.push(containers[x].Labels);
-			    retour.state.push(containers[x].State);
-			    retour.status.push(containers[x].Status);
-			    retour.hostconfig.push(containers[x].HostConfig);
-			    retour.networksettings.push(containers[x].NetworkSettings);
-			    retour.mounts.push(containers[x].Mounts);
-			}
-			break;
-		}
-	    }
-	    callback(retour);
-	});
+                        break;
+                    case "names":
+                        retour.names = [];
+                        for(x in containers) {
+                            retour.names.push(containers[x].Names[0]);
+                        }
+                        break;
+                    case "image":
+                        retour.image = [];
+                        for(x in containers) {
+                            retour.image.push(containers[x].Image);
+                        }
+                        break;
+                    case "imageid":
+                        retour.imageid = [];
+                        for(x in containers) {
+                            retour.imageid.push(containers[x].ImageID);
+                        }
+                        break; //verified to there
+                    case "command":
+                        retour.command = [];
+                        for(x in containers) {
+                            retour.command.push(containers[x].Command);
+                        }
+                        break;
+                    case "created":
+                        retour.created = [];
+                        for(x in containers) {
+                            retour.created.push(containers[x].Created);
+                        }
+                        break;
+                    case "ports":
+                        retour.ports = [];
+                        for(x in containers) {
+                            retour.ports.push(containers[x].Ports[0]);
+                        }
+                        break;
+                    case "labels":
+                        retour.labels = [];
+                        for(x in containers) {
+                            retour.labels.push(containers[x].Labels);
+                        }
+                        break;
+                    case "state":
+                        retour.state = [];
+                        for(x in containers) {
+                            retour.state.push(containers[x].State);
+                        }
+                        break;
+                    case "status":
+                        retour.status = [];
+                        for(x in containers) {
+                            retour.status.push(containers[x].Status);
+                        }
+                        break;
+                    case "hostconfig":
+                        retour.hostconfig = [];
+                        for(x in containers) {
+                            retour.hostconfig.push(containers[x].HostConfig);
+                        }
+                        break;
+                    case "networksettings":
+                        retour.networksettings = [];
+                        for(x in containers) {
+                            retour.networksettings.push(containers[x].NetworkSettings);
+                        }
+                        break;
+                    case "mounts":
+                        retour.mounts = [];
+                        for(x in containers) {
+                            retour.mounts.push(containers[x].Mounts);
+                        }
+                        break;
+                    case "*":
+                        retour = {
+                            "id":[],
+                            "names":[],
+                            "image":[],
+                            "imageid":[],
+                            "command":[],
+                            "created":[],
+                            "ports":[],
+                            "labels":[],
+                            "state":[],
+                            "status":[],
+                            "hostconfig":[],
+                            "networksettings":[],
+                            "mounts":[],
+                        }
+                        for(x in containers) {
+                            retour.id.push(containers[x].Id);
+                            retour.names.push(containers[x].Names[0]);
+                            retour.image.push(containers[x].Image);
+                            retour.imageid.push(containers[x].ImageID);
+                            retour.command.push(containers[x].Command);
+                            retour.created.push(containers[x].Created);
+                            retour.ports.push(containers[x].Ports[0]);
+                            retour.labels.push(containers[x].Labels);
+                            retour.state.push(containers[x].State);
+                            retour.status.push(containers[x].Status);
+                            retour.hostconfig.push(containers[x].HostConfig);
+                            retour.networksettings.push(containers[x].NetworkSettings);
+                            retour.mounts.push(containers[x].Mounts);
+                        }
+                        break;
+                }
+            }
+            callback(retour);
+        });
     },
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ noop = function(){};
 
 const sql = require("./modules/postgres.js"),
     docker = require("./modules/docker.js");
-    compose = require("./modules/compose.js");
+compose = require("./modules/compose.js");
 
 const express = require('express'),
     app = express(),
@@ -20,8 +20,8 @@ compose.list(console.log,'names','id');
 // compose.up(console.log,"medialog");
 
 io.sockets.on('connection', function (socket) {
-        socket.emit('message', 'Sucessfully connected');
-        });
+    socket.emit('message', 'Sucessfully connected');
+});
 
 app.set('views', __dirname+'/html/');
 app.use(express.static(__dirname+'/public/'));
@@ -30,166 +30,166 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({secret: 'Scrt'}))
 
 // WEBUI
-        .get('/:input', function(req, res, next) {
-            if(fs.existsSync(__dirname+'/html/'+req.params.input+'.ejs')){
-                res.render(req.params.input+'.ejs');
-            }
-            else{
-                next();
-            }
-        })
+    .get('/:input', function(req, res, next) {
+        if(fs.existsSync(__dirname+'/html/'+req.params.input+'.ejs')){
+            res.render(req.params.input+'.ejs');
+        }
+        else{
+            next();
+        }
+    })
 // GET API
-        .get('/api/:type', function(req, res, next) {
-            switch (req.params.type) {
-                case 'containers':
-                    docker.list(function(data){
-                        res.send(data)},'names','id','image','ports','command','state','labels');
-                    break;
-                case 'compose':
-                    compose.list(function(data){
-                        res.send(data)},'names','id','image','ports','command','state');
-                    break;
-                case 'apps':
-                    break;
-                default:
-                    next();
+    .get('/api/:type', function(req, res, next) {
+        switch (req.params.type) {
+            case 'containers':
+                docker.list(function(data){
+                    res.send(data)},'names','id','image','ports','command','state','labels');
+                break;
+            case 'compose':
+                compose.list(function(data){
+                    res.send(data)},'names','id','image','ports','command','state');
+                break;
+            case 'apps':
+                break;
+            default:
+                next();
 
-            }
+        }
 
-        })
+    })
 // POST API
-        .post('/api/:type/:action', function(req, res, next) {
-            switch (req.params.type) {
-                case 'containers':
-                    switch (req.paramas.action) {
-                        case 'start':
-                            docker.start(function (data){
-                                res.send('started');
-                            },req.body.id)
-                            break;
-                        case 'stop':
-                            docker.stop(function (data){
-                                res.send('stoped');
-                            },req.body.id)
-                            break;
-                        case 'state':
-                            docker.inspect(function (data){
-                                res.send(data.State.Status)
-                            },req.body.id)
-                            break;
-                        case 'remove':
-                            docker.remove(function (data){
-                                res.send('done');
-                            },req.body.id)
-                            break;
-                        case 'create':
-                            docker.create(function (err,container){
+    .post('/api/:type/:action', function(req, res, next) {
+        switch (req.params.type) {
+            case 'containers':
+                switch (req.paramas.action) {
+                    case 'start':
+                        docker.start(function (data){
+                            res.send('started');
+                        },req.body.id)
+                        break;
+                    case 'stop':
+                        docker.stop(function (data){
+                            res.send('stoped');
+                        },req.body.id)
+                        break;
+                    case 'state':
+                        docker.inspect(function (data){
+                            res.send(data.State.Status)
+                        },req.body.id)
+                        break;
+                    case 'remove':
+                        docker.remove(function (data){
+                            res.send('done');
+                        },req.body.id)
+                        break;
+                    case 'create':
+                        docker.create(function (err,container){
+                            if(err){
+                                res.send(err.message);
+                            }
+                            else{
+                                res.send(container.id);
+                            }
+                        },req.body.image,req.body.name)
+                        break;
+                    case 'pull':
+                        docker.pull(function (err,stream){
+                            if(err){
+                                res.send(err.message);
+                            }
+                            else{
+                                res.send("done");
+                            }
+                        },req.body.image)
+                        break;
+                    default:
+                        next();
+                }
+                break;
+            case 'compose':
+                switch (req.params.action) {
+                    case 'up':
+                        compose.create((stdout,stderr,err) => {
+                            compose.start((stdout,stderr,err) => {
                                 if(err){
-                                    res.send(err.message);
+                                    res.send("Error")
                                 }
                                 else{
-                                    res.send(container.id);
+                                    res.send("started")
                                 }
-                            },req.body.image,req.body.name)
-                            break;
-                        case 'pull':
-                            docker.pull(function (err,stream){
-                                if(err){
-                                    res.send(err.message);
-                                }
-                                else{
-                                    res.send("done");
-                                }
-                            },req.body.image)
-                            break;
-                        default:
-                            next();
-                    }
-                    break;
-                case 'compose':
-                    switch (req.params.action) {
-                        case 'up':
-                            compose.create((stdout,stderr,err) => {
-                                compose.start((stdout,stderr,err) => {
-                                    if(err){
-                                        res.send("Error")
-                                    }
-                                    else{
-                                        res.send("started")
-                                    }
-                                },req.body.name);
                             },req.body.name);
+                        },req.body.name);
 
-                            break;
-                        case 'down':
-                            compose.down(function(stdout,stderr,err){
-                                if(err){
-                                    res.send("Error")
-                                }
-                                else{
-                                    res.send("done");
-                                }
-                            },req.body.name)
-                            break;
-                        case 'start':
-                            compose.start(function(stdout,stderr,err){
-                                if(err){
-                                    res.send("Error")
-                                }
-                                else{
-                                    res.send("started");
-                                }
-                            },req.body.name)
-                            break;
-                        case 'stop':
-                            compose.stop(function(stdout,stderr,err){
-                                if(err){
-                                    res.send("Error")
-                                }
-                                else{
-                                    res.send("stoped");
-                                }
-                            },req.body.name)
-                            break;
-                        case 'create':
-                            compose.create(function(stdout,stderr,err){
-                                if(err){
-                                    res.send("Error")
-                                }
-                                else{
-                                    res.send("done");
-                                }
-                            },req.body.name)
-                            break;
-                        case 'rm':
-                            compose.rm(function(stdout,stderr,err){
-                                if(err){
-                                    res.send("Error")
-                                }
-                                else{
-                                    res.send("done");
-                                }
-                            },req.body.name)
-                            break;
-                        default:
-                            next();
-                    }
-                    break;
-                case 'apps':
-                    switch (req.paramas.action) {
-                        case 'import':
-                            break;
-                        default:
-                            next();
-                    }
-                    break;
-                default:
-                    next();
-            }
-        })
+                        break;
+                    case 'down':
+                        compose.down(function(stdout,stderr,err){
+                            if(err){
+                                res.send("Error")
+                            }
+                            else{
+                                res.send("done");
+                            }
+                        },req.body.name)
+                        break;
+                    case 'start':
+                        compose.start(function(stdout,stderr,err){
+                            if(err){
+                                res.send("Error")
+                            }
+                            else{
+                                res.send("started");
+                            }
+                        },req.body.name)
+                        break;
+                    case 'stop':
+                        compose.stop(function(stdout,stderr,err){
+                            if(err){
+                                res.send("Error")
+                            }
+                            else{
+                                res.send("stoped");
+                            }
+                        },req.body.name)
+                        break;
+                    case 'create':
+                        compose.create(function(stdout,stderr,err){
+                            if(err){
+                                res.send("Error")
+                            }
+                            else{
+                                res.send("done");
+                            }
+                        },req.body.name)
+                        break;
+                    case 'rm':
+                        compose.rm(function(stdout,stderr,err){
+                            if(err){
+                                res.send("Error")
+                            }
+                            else{
+                                res.send("done");
+                            }
+                        },req.body.name)
+                        break;
+                    default:
+                        next();
+                }
+                break;
+            case 'apps':
+                switch (req.paramas.action) {
+                    case 'import':
+                        break;
+                    default:
+                        next();
+                }
+                break;
+            default:
+                next();
+        }
+    })
 //Default
-        .use(function(req, res, next){
-                res.redirect('/main');
-        })
+    .use(function(req, res, next){
+        res.redirect('/main');
+    })
 
 server.listen(8080);
